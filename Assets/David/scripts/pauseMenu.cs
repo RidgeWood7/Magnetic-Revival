7589cf55786f8451f474b73aeeb1232258b54b35
MUKILTEO\1725383 <189248david@gmail.com>
2025-11-19 12:57:44 -0800

    pause and cinemachine

---
+++ Assets/David/scripts/pauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseScreen;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+    }
+
+    void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    void ResumeGame()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Placeholder");
+    }
+
+    public void Resume()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
