83c126b42b3a054027b90a708e943847ae6b3eaf
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2025-12-01 13:35:25 -0800

    Credits

---
+++ Assets/John/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Method to load a scene by its name
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Optional: Method to load a scene by its build index
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+ 
+}
+
+
